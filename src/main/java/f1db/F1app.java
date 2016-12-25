31ec9abbc4c77b8b6de3b18479eb3e5ac898843d
ConstantKrieg <kim.toivonen@outlook.com>
2016-12-25 14:10:06 +0200

    pom.xml started and a main class created

---
+++ src/main/java/f1db/F1app.java
@@ -0,0 +1,12 @@
+
+package f1db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1app{
+   public static void main(String[] args) throws Exception {
+        SpringApplication.run(F1app.class, args);
+    }
+}
